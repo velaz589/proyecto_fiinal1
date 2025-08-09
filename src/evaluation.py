f76ae64236a73d0b12de7ab82c70e3e650c77b25
velaz589 <z.velazquez@hotmail.com>
2025-08-09 21:27:16 +0200

    Update evaluation.py

--- src/evaluation.py
+++ src/evaluation.py
@@ -3,8 +3,5 @@ evaluation.py
 Evaluación del modelo y generación de métricas.
 """
 
-# Aquí va el código de evaluación del modelo a parte del codigo de evaluacion se incluiran en este 
-# archivo el entrenamiento de los modelos. 
-# de forma experimental se hara uso de las clases para ordenar el codigo. 
-
-
+# Aquí va el código de evaluación del modelo
+# incluiremos aqui el codigo con los distintos modelos.
