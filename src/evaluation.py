a79dfac1136a7ddb9278a4989b9b01ac429facd9
velaz589 <z.velazquez@hotmail.com>
2025-08-11 21:38:49 +0200

    cosas

--- src/evaluation.py
+++ src/evaluation.py
@@ -11,6 +11,7 @@ from sklearn.metrics import classification_report
 
 from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score,\
                             roc_auc_score, roc_curve, precision_recall_curve, confusion_matrix
+import fuentes as ft
 
 
 def guardar_modelo(nombre:str,modelo):
@@ -33,21 +34,23 @@ def metricas(clf,y_test,predictions=False,X_test):
     else:
         preds = clf.predict(X_test)
 
-    print("Score del modelo (accuracy):", round(clf.score(y_test.values,preds), 3))
+    preds = clf.predict(X_test)
+
+    print("Score del modelo (accuracy):", round(clf.score(X_test,y_test), 3))
     print("Accuracy score:", round(accuracy_score(preds, y_test), 3))
     print("Recall score:", round(recall_score(preds, y_test), 3))
     print("Precision score:", round(precision_score(preds, y_test), 3))
     print("F1 score:", round(f1_score(preds, y_test), 3))
     print("AUC:", round(roc_auc_score(preds, y_test), 3))
 
-    y_pred_prob = clf.predict_proba(X_test)
-    fpr, tpr, thresholds = roc_curve(y_test.values, y_pred_prob)
-    plt.plot(fpr, tpr)
-    plt.xlim([0.0, 1.0])
-    plt.ylim([0.0, 1.0])
-    plt.title('ROC curve for titanic classifier')
-    plt.xlabel('False Positive Rate (1 - Specificity)')
-    plt.ylabel('True Positive Rate (Sensitivity)')
-    plt.grid(True)
+    y_pred_prob = clf.predict_proba(X_test)[:, 1]
 
+    fpr, tpr, thresholds=roc_curve(y_test,y_pred_prob)
+    ft.plt.plot(fpr, tpr)
+    ft.plt.xlim([0.0, 1.0])
+    ft.plt.ylim([0.0, 1.0])
+    ft.plt.title('ROC curve for client-complaint classifier')
+    ft.plt.xlabel('False Positive Rate (1 - Specificity)')
+    ft.plt.ylabel('True Positive Rate (Sensitivity)')
+    ft.plt.grid(True)
     
