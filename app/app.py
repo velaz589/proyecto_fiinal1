0e701078e81478bef8f38f702fe53348cd14564a
velaz589 <z.velazquez@hotmail.com>
2025-08-15 18:29:10 +0200

    app

--- app/app.py
+++ app/app.py
@@ -4,4 +4,10 @@ Aplicación web para el modelo entrenado final.
 """
 
 # Aquí va el código de la aplicación web
+import gradio as gr
+import sys
+sys.path.append('../notebooks')
+
+import fuentes as ft 
+
 
